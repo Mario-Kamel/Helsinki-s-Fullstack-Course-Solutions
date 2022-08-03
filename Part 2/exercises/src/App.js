08528328dd3502c69ec2b7b6a6524cd9ad55f0bd
Mario Kamel <mario.medhat99@gmail.com>
2022-08-04 01:10:37 +0200

    Exercises 2.6-2.8

--- Part 2/exercises/src/App.js
+++ Part 2/exercises/src/App.js
@@ -1,52 +1,55 @@
-import Course from "./components/Course"
-const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
+import { useState } from 'react'
 
-  return( 
-    courses.map(course => <Course key = {course.id} course={course}/>)
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber,setNewNumber] = useState('')
+  const handleNameChange = (event)=>{
+    setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) =>{
+    setNewNumber(event.target.value)
+  }
+  const handleFormSubmit = (event) =>{
+    event.preventDefault()
+    const alreadyExists = persons.find(person => person.name ===newName)
+    if(!alreadyExists){
+      const newPerson = {
+        name:newName,
+        number:newNumber,
+        id:persons.length+1
+      }
+      setPersons(persons.concat(newPerson))
+      setNewName('')
+      setNewNumber('')
+    }
+    else{
+      window.alert(`${newName} is already added to phonebook`)
+    }
+  }
+  
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          name: <input value={newName} onChange = {handleNameChange}/>
+          <br/>
+          number: <input value={newNumber} onChange = {handleNumberChange}/>
+        </div>
+        <div>
+          <button type="submit" >add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      
+      {persons.map(person => <p key={person.name}>{person.name}  {person.number}</p>)}
+    </div>
   )
 }
 

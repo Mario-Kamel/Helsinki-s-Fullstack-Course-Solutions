16c215239e4ccb147317b272f17c7367f1d4336b
Mario Kamel <mario.medhat99@gmail.com>
2022-08-09 20:36:38 +0200

    Exercise 3.9

--- Part 3/exercises/index.js
+++ Part 3/exercises/index.js
@@ -1,10 +1,9 @@
 const express = require('express')
-
 const app = express()
-const cors = require('cors')
-app.use(cors())
 app.use(express.json())
 
+const cors = require('cors')
+app.use(cors())
 const morgan = require('morgan')
 
 
